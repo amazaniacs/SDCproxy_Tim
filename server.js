faccb4da252c23459458e724f34eb856ac382047
Tingbin Huang <tingbinhuang@Tingbins-MacBook-Pro.local>
2019-03-13 10:51:20 -0700

    Add and update files

---
+++ server.js
@@ -0,0 +1,14 @@
+const nr = require("newrelic");
+const express = require('express');
+const compression = require('compression');
+const app = express();
+
+var PORT = 8081;
+app.use(compression());
+app.use(express.static(`${__dirname}/client/dist`));
+
+app.get('*', (req, res) => {
+  res.sendFile(`${__dirname}/client/dist/index.html`);
+});
+
+app.listen(PORT, console.log(`listening at ${PORT}`));
